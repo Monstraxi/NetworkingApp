4de9a5bd0d0eba5192d8bfb017b9b6e5f2191aa9
Rempaah <rmuhammadreff@gmail.com>
2023-09-11 21:00:03 +0700

    getaddrin() implementation

--- NetworkApp/main.cpp
+++ NetworkApp/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <winsock2.h>
+#include <WS2tcpip.h>
 
 int main()
 {
@@ -16,4 +16,47 @@ int main()
 		WSACleanup();
 		return -1;
 	}
+
+	int status;
+	addrinfo hints;
+	addrinfo* servInfo;
+	memset(&hints, 0, sizeof(hints));
+
+	char ipstr[INET6_ADDRSTRLEN];
+
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	status = getaddrinfo("www.youtube.com", NULL, &hints, &servInfo);
+
+	if (status != 0)
+	{
+		std::cerr << "getaddrinfo error: " << gai_strerror(status) << std::endl;
+		return -1;
+	}
+
+	for (addrinfo* p = servInfo; p != nullptr; p = p->ai_next)
+	{
+		void* addr;
+		char* ipVer;
+
+		if (p->ai_family == AF_INET)
+		{
+			sockaddr_in* ipV4 = (sockaddr_in*)p->ai_addr;
+			addr = &(ipV4->sin_addr);
+			ipVer = "IPv4";
+		}
+		else
+		{
+			sockaddr_in6* ipV6 = (sockaddr_in6*)p->ai_addr;
+			addr = &(ipV6->sin6_addr);
+			ipVer = "IPv6";
+		}
+
+		inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
+		std::cout << ipVer << ": " << ipstr << std::endl;
+	}
+
+	WSACleanup();
 }
